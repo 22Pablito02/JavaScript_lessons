fa2e1c3d9f1feaf054fbd030f4157bb4673e10fc
Pablito <PavelVolkovPro@yandex.ru>
2023-09-03 20:13:55 +0300

    Home_work_1

---
+++ HW_1/index.js
@@ -0,0 +1,11 @@
+// Задание №1
+
+let celsius = 36.6;
+console.log(`${celsius} градусов цельсия - это ${(9 / 5) * celsius + 32} градусов фарингейта`);
+
+
+// Задание №2
+
+const name = "Pavel";
+let admin = name;
+console.log(`Имя администратора: ${admin}`); 
