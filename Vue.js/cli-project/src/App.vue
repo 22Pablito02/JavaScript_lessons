6539df83d3ff289e4159d146da63c1f6e5e0fb3e
Pablito <PavelVolkovPro@yandex.ru>
2023-10-23 17:54:34 +0300

    Home work 4

---
+++ Vue.js/cli-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <home />
+  <blog />
+  <articles />
+</template>
+
+<script>
+import home from "@/components/home.vue"
+import blog from "@/components/blog.vue"
+import articles from "@/components/articles.vue"
+export default {
+  components: {
+    home,
+    blog,
+    articles,
+  }
+}
+</script>
+
+
+<style></style>
