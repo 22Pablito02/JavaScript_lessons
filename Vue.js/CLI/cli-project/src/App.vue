c79206cf13e49a9de6d7aab4ec9b2335d9a1694d
Pablito <PavelVolkovPro@yandex.ru>
2023-11-01 15:48:37 +0300

    Home work 7

--- Vue.js/CLI/cli-project/src/App.vue
+++ Vue.js/CLI/cli-project/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <detailsP />
   <homeP />
   <blogP />
   <articlesP />
   <projectsP />
+  
 </template>
 
 <script>
@@ -10,13 +12,15 @@ import homeP from "@/components/homeP.vue"
 import blogP from "@/components/blogP.vue"
 import articlesP from "@/components/articlesP.vue"
 import projectsP from "./components/projectsP.vue";
+import detailsP from "./components/detailsP.vue";
 export default {
   components: {
     homeP,
     blogP,
     articlesP,
-    projectsP
-  }
+    projectsP,
+    detailsP
+}
 }
 </script>
 
